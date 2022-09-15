9827ba88e9b5e26c5629f9fdfb7364b4d4e0d044
Zhonghu Xu <xuzhonghu@huawei.com>
2022-09-15 15:55:51 +0800

    Fix destination rule selection without considering service namespace

--- pilot/pkg/networking/core/v1alpha3/networkfilter.go
+++ pilot/pkg/networking/core/v1alpha3/networkfilter.go
@@ -230,7 +230,7 @@ func buildOutboundNetworkFilters(node *model.Proxy,
 	service := push.ServiceForHostname(node, host.Name(routes[0].Destination.Host))
 	var destinationRule *networking.DestinationRule
 	if service != nil {
-		destinationRule = CastDestinationRule(node.SidecarScope.DestinationRule(model.TrafficDirectionOutbound, node, service.Hostname).GetRule())
+		destinationRule = CastDestinationRule(node.SidecarScope.DestinationRule(model.TrafficDirectionOutbound, node, service).GetRule())
 	}
 	if len(routes) == 1 {
 		clusterName := istioroute.GetDestinationCluster(routes[0].Destination, service, port.Port)
